ddc87427a3a0cf4694f2c61ff6ba065c67831e07
Supratim Dey <supratimdey.2014@gmail.com>
2023-03-20 22:22:23 +0530

    getting started with React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs"; // this import is important
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+      {/* <h1 className="text-2xl font-bold text-red-500">Hello world!</h1>
+      <UilReact size={50}/> */}
+    </div> 
+  );
+}
+
+export default App;
